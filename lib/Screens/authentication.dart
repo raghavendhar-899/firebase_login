9c8fc7c244fec98fa961771d22faca534051eaff
raghavendhar-899 <adusumilliraghavendhar2000@gmail.com>
2020-05-09 22:40:43 +0530

    added email and password signin/signup

--- lib/Screens/authentication.dart
+++ lib/Screens/authentication.dart
@@ -1,9 +1,25 @@
+import 'package:firebase1/Screens/register.dart';
 import 'package:firebase1/Screens/signin.dart';
 import 'package:flutter/material.dart';
 
-class Authentication extends StatelessWidget {
+
+class Authentication extends StatefulWidget {
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  bool showSighIn=true;
+  void toggleView(){
+    setState(() => showSighIn=!showSighIn);
+  }
   @override
   Widget build(BuildContext context) {
-    return SignIn();
+    if(showSighIn){
+    return SignIn(toggleView: toggleView);
+    }
+    else{
+    return Register(toggleView: toggleView);
+    }
   }
 }
--- lib/Screens/signin.dart
+++ lib/Screens/signin.dart
@@ -2,39 +2,93 @@ import 'package:firebase1/Services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+  final Function toggleView;
+  SignIn({this.toggleView});
+
   @override
   _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
-
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // TextField state
+  String email = '', password = '', e = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(25, 25, 25, 1),
+        backgroundColor: Colors.black,
         title: Text("Sign In"),
+        actions: <Widget>[
+          FlatButton.icon(
+            textColor: Colors.white60,
+            icon: Icon(Icons.person),
+            label: Text("Register"),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
-        child: RaisedButton(
-          child: Text("Sign in as guest"),
-          onPressed: ()async{
-             dynamic result = await _auth.signInAnon();
-
-             if(result==null)
-             {
-               print("error signin");
-             }
-             else
-             {
-               print("signin succes = ${result.uid}");
-             }
-          },
-        )
+        child: Form(
+          key: _formKey,
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              validator: (value) =>
+                  value.contains("@") ? null : "Enter Valid Email",
+              decoration: InputDecoration(
+                hintText: "Email",
+                filled: true,
+                fillColor: Colors.black26,
+                ),
+              onChanged: (value) {
+                setState(() {
+                  email = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              validator: (value) => value.isEmpty ? "Enter password" : null,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Passw ord"),
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
+                color: Colors.black,
+                child: Text("Sign in", style: TextStyle(color: Colors.white)),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if(result==null)
+                    {
+                      e='could not signin';
+                    }
+                    else{
+                      
+                    }
+                  }
+                }),
+            SizedBox(height: 20),
+            Text(
+              e,
+              style: TextStyle(color: Colors.red),
+            ),
+          ]),
+        ),
       ),
     );
   }
